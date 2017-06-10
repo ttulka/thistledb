cb2057bd33d83c48a3d862fd88bdc69b7ca9028f
Tomas Tulka <tomas.tulka@gmail.com>
2017-06-10 10:45:26 +0200

    dummy data source impl

--- server/src/main/java/cz/net21/ttulka/thistledb/server/db/DataSourceImpl.java
+++ server/src/main/java/cz/net21/ttulka/thistledb/server/db/DataSourceImpl.java
@@ -1,6 +1,8 @@
 package cz.net21.ttulka.thistledb.server.db;
 
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.json.JSONObject;
 
@@ -17,17 +19,19 @@ import reactor.core.publisher.Flux;
 @CommonsLog
 public class DataSourceImpl implements DataSource {
 
+    private List<JSONObject> db = new ArrayList<>();
+
     public DataSourceImpl(Path dataDir) {
         // TODO
     }
 
     @Override
     public Flux<JSONObject> select(@NonNull String collectionName, @NonNull String columns, String where) {
-        return null;
+        return Flux.fromIterable(db);
     }
 
     @Override
     public void insert(@NonNull String collectionName, @NonNull JSONObject data) {
-
+        db.add(data);
     }
 }
