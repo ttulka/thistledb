6f63c2d5049d30682e1a8aa7e4b06fcdc5847d98
Tomas Tulka <tomas.tulka@gmail.com>
2017-06-14 18:35:16 +0200

    update validator

--- server/src/main/java/cz/net21/ttulka/thistledb/server/Validator.java
+++ server/src/main/java/cz/net21/ttulka/thistledb/server/Validator.java
@@ -8,9 +8,14 @@ import java.util.regex.Pattern;
 // TODO
 class Validator {
 
-    private static final Pattern SELECT = Pattern.compile("SELECT\\s(\\*|[\\w\\d,\\s]+)\\sFROM\\s([\\w\\d]+)(\\sWHERE\\s([\\(\\)\\w\\d\\=\\s\\']+))?");
-    private static final Pattern INSERT = Pattern.compile("INSERT\\sINTO\\s([\\w\\d]+)\\sVALUES\\s(\\{.+\\})");
-    // TODO
+    static final Pattern SELECT = Pattern.compile("SELECT\\s(\\*|[\\w\\d,\\s]+)\\sFROM\\s([\\w\\d]+)(\\sWHERE\\s([()\\w\\d=\\s']+))?");
+    static final Pattern INSERT = Pattern.compile("INSERT\\sINTO\\s([\\w\\d]+)\\sVALUES\\s(\\{.+\\})");
+    static final Pattern UPDATE = Pattern.compile("UPDATE\\s([\\w\\d]+)\\sSET\\s(((?!WHERE).)+)(\\s)?=(\\s)?(((?!WHERE).)+)(\\sWHERE\\s([()\\w\\d=\\s']+))?");
+    static final Pattern DELETE = Pattern.compile("DELETE\\sFROM\\s([\\w\\d]+)\\s(WHERE\\s([()\\w\\d=\\s\']+))?");
+    static final Pattern CREATE = Pattern.compile("CREATE\\s^(INDEX)([\\w\\d]+)");
+    static final Pattern DROP = Pattern.compile("DROP\\s^(INDEX)([\\w\\d]+)");
+    static final Pattern CREATE_INDEX = Pattern.compile("CREATE\\sINDEX\\s(.+)\\sON\\s([\\w\\d]+)");
+    static final Pattern DROP_INDEX = Pattern.compile("DROP\\sINDEX\\s(.+)\\sON\\s([\\w\\d]+)");
 
     private final String ql;
 
@@ -20,6 +25,12 @@ class Validator {
 
     public boolean validate() {
         return SELECT.matcher(ql).matches() ||
-               INSERT.matcher(ql).matches();
+               INSERT.matcher(ql).matches() ||
+               UPDATE.matcher(ql).matches() ||
+               DELETE.matcher(ql).matches() ||
+               CREATE.matcher(ql).matches() ||
+               DROP.matcher(ql).matches() ||
+               CREATE_INDEX.matcher(ql).matches() ||
+               DROP_INDEX.matcher(ql).matches();
     }
 }
--- server/src/test/java/cz/net21/ttulka/thistledb/server/ValidatorTest.java
+++ server/src/test/java/cz/net21/ttulka/thistledb/server/ValidatorTest.java
@@ -11,10 +11,14 @@ import static org.hamcrest.MatcherAssert.assertThat;
 public class ValidatorTest {
 
     @Test
-    public void validateSelectTest() {
+    public void validateInsertWrongTest() {
         assertThat(new Validator(null).validate(), is(false));
         assertThat(new Validator("").validate(), is(false));
         assertThat(new Validator("xxx").validate(), is(false));
+    }
+
+    @Test
+    public void validateSelectTest() {
         assertThat(new Validator("SELECT").validate(), is(false));
         assertThat(new Validator("SELECT * FROM").validate(), is(false));
         assertThat(new Validator("SELECT FROM").validate(), is(false));
@@ -38,9 +42,6 @@ public class ValidatorTest {
 
     @Test
     public void validateInsertTest() {
-        assertThat(new Validator(null).validate(), is(false));
-        assertThat(new Validator("").validate(), is(false));
-        assertThat(new Validator("xxx").validate(), is(false));
         assertThat(new Validator("INSERT").validate(), is(false));
         assertThat(new Validator("INSERT INTO").validate(), is(false));
         assertThat(new Validator("INSERT INTO test").validate(), is(false));
@@ -52,5 +53,55 @@ public class ValidatorTest {
         assertThat(new Validator("INSERT INTO test VALUES {\"a1\" : { \"a_2\":\"1\" }}").validate(), is(true));
         assertThat(new Validator("INSERT INTO test VALUES {\"a1\" : { \"a_2\": [\"1\", \"a_2\" ] }}").validate(), is(true));
         assertThat(new Validator("INSERT INTO test VALUES {\"a1\" : { \"a_2\": [{\"1\" : \"a_2\"} , {\"1_a\" : \"_a_2_.1\"} ] }}").validate(), is(true));
+    }
+
+    @Test
+    public void validateUpdateTest() {
+        assertThat(new Validator("UPDATE").validate(), is(false));
+        assertThat(new Validator("UPDATE *").validate(), is(false));
+        assertThat(new Validator("UPDATE test").validate(), is(false));
+        assertThat(new Validator("UPDATE test SET").validate(), is(false));
+        assertThat(new Validator("UPDATE test SET WHERE").validate(), is(false));
+        assertThat(new Validator("UPDATE test SET x.a_1 WHERE").validate(), is(false));
+        assertThat(new Validator("UPDATE test SET x.a_1 WHERE 1=1").validate(), is(false));
+        assertThat(new Validator("UPDATE test SET x.a_1 = WHERE 1=1").validate(), is(false));
+        assertThat(new Validator("UPDATE * SET x.a_1 = 1 WHERE 1=1").validate(), is(false));
+
+        assertThat(new Validator("UPDATE test SET x.a_1 = 1").validate(), is(true));
+        assertThat(new Validator("UPDATE test SET x.a_1 = '1'").validate(), is(true));
+        assertThat(new Validator("UPDATE test SET x.a_1 = y.a_1").validate(), is(true));
+        assertThat(new Validator("UPDATE test SET x.a_1 = 'y.a_1'").validate(), is(true));
+        assertThat(new Validator("UPDATE test SET x.a_1 = 'y.a_1' WHERE 1").validate(), is(true));
+        assertThat(new Validator("UPDATE test SET x.a_1 = 'y.a_1' WHERE 1 AND 1").validate(), is(true));
+        assertThat(new Validator("UPDATE test SET x.a_1 = 'y.a_1' WHERE 1 AND 1 OR 1").validate(), is(true));
+        assertThat(new Validator("UPDATE test SET x.a_1 = 'y.a_1' WHERE ((1 AND 1) OR 1)").validate(), is(true));
+        assertThat(new Validator("UPDATE test SET x.a_1 = 'y.a_1' WHERE ((1 AND 1) OR (1 OR 1))").validate(), is(true));
+        assertThat(new Validator("UPDATE test SET x.a_1 = 'y.a_1' WHERE ((1 AND 1) OR (1 OR 1)) AND 1").validate(), is(true));
+        assertThat(new Validator("UPDATE test SET x.a_1 = 'y.a_1' WHERE ((a1=1 AND a_2 = '') OR (a1 = 'xxx' OR 1)) AND 1").validate(), is(true));
+    }
+
+    @Test
+    public void validateDeleteTest() {
+        // TODO
+    }
+
+    @Test
+    public void validateCreateIndexTest() {
+        // TODO
+    }
+
+    @Test
+    public void validateDropIndexTest() {
+        // TODO
+    }
+
+    @Test
+    public void validateCreateTest() {
+        // TODO
+    }
+
+    @Test
+    public void validateDropTest() {
+        // TODO
     }
 }
