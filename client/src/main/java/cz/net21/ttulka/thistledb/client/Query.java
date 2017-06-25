d89155a1af7f63e0450ff072244afedaf7db14d3
Tomas Tulka <tomas.tulka@gmail.com>
2017-06-25 18:46:39 +0200

    query select test

--- client/src/main/java/cz/net21/ttulka/thistledb/client/Query.java
+++ client/src/main/java/cz/net21/ttulka/thistledb/client/Query.java
@@ -25,25 +25,32 @@ public class Query {
         return nativeQuery;
     }
 
-    private static class QueryBuilder {
+    public static class QueryBuilder {
 
         private StringBuilder sb = new StringBuilder();
 
         private boolean command;
-        private boolean select;
         private boolean setAllowed;
         private boolean whereAllowed;
         private boolean where;
+        private boolean ready;
+
+        private QueryBuilder() {
+            // private access
+        }
 
         public Query build() {
+            if (!ready) {
+                throw new IllegalStateException("Query is not ready.");
+            }
             return new Query(sb.toString());
         }
 
         public QueryBuilder select(String from) {
-            return select(new String[]{"*"}, from);
+            return select(from, new String[]{"*"});
         }
 
-        public QueryBuilder select(String[] columns, String from) {
+        public QueryBuilder select(String from, String[] columns) {
             if (columns == null || columns.length == 0) {
                 throw new IllegalArgumentException("Columns cannot be empty.");
             }
@@ -54,17 +61,83 @@ public class Query {
                 throw new IllegalStateException("Query has already a command.");
             }
             command = true;
-            select = true;
 
             sb.append("SELECT ");
             Arrays.stream(columns).forEach(s -> {
-                if (!"SELECT".equals(sb.toString())) {
+                if (!"SELECT ".equals(sb.toString())) {
                     sb.append(", ");
                 }
                 sb.append(s);
             });
             sb.append(" FROM ").append(from);
             whereAllowed = true;
+            ready = true;
+            return this;
+        }
+
+        public QueryBuilder insert(String into, JSONObject data) {
+            if (data == null) {
+                throw new IllegalArgumentException("JSON data cannot be null.");
+            }
+            return insert(into, data.toString());
+        }
+
+        public QueryBuilder insert(String into, String data) {
+            if (into == null || into.isEmpty()) {
+                throw new IllegalArgumentException("Collection name cannot be empty.");
+            }
+            if (data == null || data.isEmpty()) {
+                throw new IllegalArgumentException("JSON data cannot be empty.");
+            }
+            if (command) {
+                throw new IllegalStateException("Query has already a command.");
+            }
+            command = true;
+            ready = true;
+            sb.append("INSERT INTO ").append(into).append(" ").append(data);
+            return this;
+        }
+
+        public QueryBuilder update(String collectionName) {
+            if (collectionName == null || collectionName.isEmpty()) {
+                throw new IllegalArgumentException("Collection name cannot be empty.");
+            }
+            if (command) {
+                throw new IllegalStateException("Query has already a command.");
+            }
+            command = true;
+            setAllowed = true;
+            sb.append("UPDATE ").append(collectionName).append(" SET");
+            return this;
+        }
+
+        public QueryBuilder set(String column, String value) {
+            if (column == null || column.isEmpty()) {
+                throw new IllegalArgumentException("Column name cannot be empty.");
+            }
+            if (value == null || value.isEmpty()) {
+                throw new IllegalArgumentException("Value cannot be empty.");
+            }
+            if (!setAllowed) {
+                throw new IllegalStateException("SET clause not expected.");
+            }
+            whereAllowed = true;
+            ready = true;
+            sb.append(" ").append(column).append("=").append(value);
+            return this;
+        }
+
+        public QueryBuilder delete(String from) {
+            if (from == null || from.isEmpty()) {
+                throw new IllegalArgumentException("Collection name cannot be empty.");
+            }
+            if (command) {
+                throw new IllegalStateException("Query has already a command.");
+            }
+            command = true;
+            whereAllowed = true;
+            ready = true;
+            sb.append("DELETE FROM ").append(from);
             return this;
         }
 
@@ -116,69 +189,6 @@ public class Query {
             return this;
         }
 
-        public QueryBuilder insert(String into, JSONObject data) {
-            if (data == null) {
-                throw new IllegalArgumentException("JSON data cannot be null.");
-            }
-            return insert(into, data.toString());
-        }
-
-        public QueryBuilder insert(String into, String data) {
-            if (into == null || into.isEmpty()) {
-                throw new IllegalArgumentException("Collection name cannot be empty.");
-            }
-            if (data == null || data.isEmpty()) {
-                throw new IllegalArgumentException("JSON data cannot be empty.");
-            }
-            if (command) {
-                throw new IllegalStateException("Query has already a command.");
-            }
-            command = true;
-            sb.append("INSERT INTO ").append(into).append(" ").append(data);
-            return this;
-        }
-
-        public QueryBuilder update(String collectionName) {
-            if (collectionName == null || collectionName.isEmpty()) {
-                throw new IllegalArgumentException("Collection name cannot be empty.");
-            }
-            if (command) {
-                throw new IllegalStateException("Query has already a command.");
-            }
-            command = true;
-            setAllowed = true;
-            sb.append("UPDATE ").append(collectionName).append(" SET");
-            return this;
-        }
-
-        public QueryBuilder set(String column, String value) {
-            if (column == null || column.isEmpty()) {
-                throw new IllegalArgumentException("Column name cannot be empty.");
-            }
-            if (value == null || value.isEmpty()) {
-                throw new IllegalArgumentException("Value cannot be empty.");
-            }
-            if (!setAllowed) {
-                throw new IllegalStateException("SET clause not expected.");
-            }
-            whereAllowed = true;
-            sb.append(" ").append(column).append("=").append(value);
-            return this;
-        }
-
-        public QueryBuilder delete(String from) {
-            if (from == null || from.isEmpty()) {
-                throw new IllegalArgumentException("Collection name cannot be empty.");
-            }
-            if (command) {
-                throw new IllegalStateException("Query has already a command.");
-            }
-            command = true;
-            whereAllowed = true;
-            sb.append("DELETE FROM ").append(from);
-            return this;
-        }
-
         public QueryBuilder createCollection(String collectionName) {
             if (collectionName == null || collectionName.isEmpty()) {
                 throw new IllegalArgumentException("Collection name cannot be empty.");
@@ -187,6 +197,7 @@ public class Query {
                 throw new IllegalStateException("Query has already a command.");
             }
             command = true;
+            ready = true;
             sb.append("CREATE ").append(collectionName);
             return this;
         }
@@ -199,6 +210,7 @@ public class Query {
                 throw new IllegalStateException("Query has already a command.");
             }
             command = true;
+            ready = true;
             sb.append("DROP ").append(collectionName);
             return this;
         }
@@ -211,6 +223,7 @@ public class Query {
                 throw new IllegalStateException("Query has already a command.");
             }
             command = true;
+            ready = true;
             sb.append("CREATE INDEX ").append(column).append(" ON ").append(collectionName);
             return this;
         }
@@ -223,6 +236,7 @@ public class Query {
                 throw new IllegalStateException("Query has already a command.");
             }
             command = true;
+            ready = true;
             sb.append("DROP INDEX ").append(column).append(" ON ").append(collectionName);
             return this;
         }
--- client/src/test/java/cz/net21/ttulka/thistledb/client/QueryTest.java
+++ client/src/test/java/cz/net21/ttulka/thistledb/client/QueryTest.java
@@ -1,9 +1,32 @@
 package cz.net21.ttulka.thistledb.client;
 
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.CoreMatchers.is;
+
 /**
  * Created by ttulka
  */
 public class QueryTest {
+
+    @Test
+    public void selectTest() {
+        Query q = Query.builder().select("test").build();
+        assertThat(q.getNativeQuery(), is("SELECT * FROM test"));
+
+        q = Query.builder().select("test", new String[]{"a1"}).build();
+        assertThat(q.getNativeQuery(), is("SELECT a1 FROM test"));
+
+        q = Query.builder().select("test").where("1", "1").build();
+        assertThat(q.getNativeQuery(), is("SELECT * FROM test WHERE 1='1'"));
+
+        q = Query.builder().select("test").where("1", "1").and("2", "2").build();
+        assertThat(q.getNativeQuery(), is("SELECT * FROM test WHERE 1='1' AND 2='2'"));
+
+        q = Query.builder().select("test").where("1", "1").and("2", "2").or("3", "3").build();
+        assertThat(q.getNativeQuery(), is("SELECT * FROM test WHERE 1='1' AND 2='2' OR 3='3'"));
+    }
 
     // TODO
 }
