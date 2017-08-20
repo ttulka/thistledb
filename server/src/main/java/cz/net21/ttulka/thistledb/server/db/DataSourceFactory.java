4cd68d3de241cb9ed777e4d82a85414015b5180b
Tomas Tulka <tomas.tulka@gmail.com>
2017-08-20 12:18:45 +0200

    data source factory added

---
+++ server/src/main/java/cz/net21/ttulka/thistledb/server/db/DataSourceFactory.java
@@ -0,0 +1,21 @@
+package cz.net21.ttulka.thistledb.server.db;
+
+import java.nio.file.Path;
+
+import lombok.NonNull;
+
+/**
+ * Data Source Factory.
+ *
+ * Created by ttulka
+ *
+ */
+public class DataSourceFactory {
+
+    private DataSourceFactory() {
+    }
+
+    public static DataSource getDataSource(@NonNull Path dataDir) {
+        return new DataSourceFile(dataDir);
+    }
+}
